6bb7a0765893cb9378190869d05dab26e0c60f4e
kiruiaaron <kiruiaaron471@gmail.com>
2023-05-12 08:50:49 +0300

    A js file to dtermine if a certain number exist in acertain range

---
+++ range.js
@@ -0,0 +1,14 @@
+function numberRange(num) {
+    if (num >= 50 && num <= 100) 
+       {
+      return true;
+       } 
+      else 
+       {
+      return false;
+    }
+  }
+  
+  console.log(numberRange(44));
+  console.log(numberRange(70));
+  console.log(numberRange(50));
